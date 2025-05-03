ebb3b9c1cdde8d1ce471fd916d4fa8d62fa2e0b0
Ximing Luo <ximluo@gmail.com>
2025-05-02 20:43:57 -0400

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,8 +11,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <CustomCursor /> 
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <CustomCursor /> 
+        <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
